6b34276e65ea42ed79d2c8dd383cc25c6fe48f27
Danil <parusovdanik@gmail.com>
2023-04-20 22:25:00 +0300

    docstring and func added

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
+
+"""Функция которая делает все буквы заглавными"""
 def all_high(value):
     return str.upper(value)
+
+"""Функция которая делает первую букву заглавной"""
+def first_high(value):
+    return str.upper(value[0]) + value[1::]
+
